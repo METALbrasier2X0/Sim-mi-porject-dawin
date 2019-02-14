768ef52cbf11e2dbe7ff1a287bc52a8c52ec3d30
Benjamin Jusserand <benjamin.jusserand@gmail.com>
2019-02-14 10:56:48 +0100

    add modal

---
+++ public/js/modal.js
@@ -0,0 +1,48 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  close_modal();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+
+function open_modal(m,exit_on_click) {
+    //titre
+    $('.modal-header').children("h2").html(m.header);
+    //corps
+    $('.modal-body').html(m.text);
+    //$('.modal-footer').children("h3").html(footer);
+    $('.modal-footer').html('');
+    console.log(m.buttons);
+    //Afficher chaque boutons
+    m.buttons.forEach(element => {
+        var $input = $('<input type="button" value="'+ element.t +'" />');
+        $input.click(element.f);
+        $input.appendTo($('.modal-footer'));
+    });
+    
+
+    modal.style.display = "block";
+
+    //si exit_on_click, ajouter un listener
+    if (exit_on_click) {
+        window.onclick = function(event) {
+            if (event.target == modal) {
+              close_modal();
+            }
+          }
+          
+    }
+    else {
+        window.onclick = function(event) {}
+    }
+}
+
+function close_modal() {
+    modal.style.display = "none";
+}
