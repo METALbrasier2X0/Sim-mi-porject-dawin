563af18b5a2dbb32cf94a34fe1056b3e7457a708
Benjamin Jusserand <benjamin.jusserand@gmail.com>
2019-02-21 11:46:49 +0100

    begin send score

--- src/Controller/ScoreController.php
+++ src/Controller/ScoreController.php
@@ -9,6 +9,7 @@ use App\Entity\User;
 use App\Repository\ScoreRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class ScoreController extends Controller
 {
@@ -23,8 +24,20 @@ class ScoreController extends Controller
     /**
      * @Route("/saveScore", name="saveScore", methods={"GET","POST"})
      */
-    public function saveScore()
+    public function saveScore(ObjectManager $em)
     {
-        return;
+        $temp = new Score();
+
+        $temp->setIsUser($this->getUser());
+        $temp->setSatisfactionC(25);
+        $temp->setReputationP(10);
+        $temp->setReputationE(10);
+        $temp->setCreation_Partie(new \DateTime());
+
+
+        $em->persist($temp);
+        $em->flush();
+
+        return new Response("Saved!");
     }
 }
